e36d55d6d0f2365d39122d25be6475b1f0a89a2f
“Joe <joseph.sherman@Loop.colum.edu>
2018-11-15 21:29:06 -0600

    create app. add 3 tasks to show on browser.

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <ol>
+      <h1>{{ message }}</h1>
+      <li>{{ task1 }}</li>
+      <li>{{ task2 }}</li>
+      <li> {{ task3 }} </li>
+  </ol>
+  </div>
+</template>
+
+
+<style>
+ol {
+  padding: 100px;
+}
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Complete the following tasks!",
+      task1: "Wash your car",
+      task2: "Do the dishes",
+      task3: "Do laundry."
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+</script>
